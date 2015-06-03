ea26f7e6608051dd300a55b06d10067b6c545058
gabistoufly <deschand.gabriel@gmail.com>
2015-06-03 17:34:44 +0200

    Create WebApp AngularJS directory + index  template

---
+++ webapp/app/controller/mainController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Gabi on 08/05/2015.
+ */
+function MainController($scope) {
+
+}
+
+$(document).ready(function(){
+    $("main").css( "marginTop", $(this).find(".navbar").css( "height" ) );
+});
