51f9fcf4c23365b3f6a52c9d48dbc198671e03f6
arjuna <arjuna-goutier@outlook.com>
2015-07-23 08:54:11 +0200

    fixed some displaying bug

--- webapp/app/js/services/services.js
+++ webapp/app/js/services/services.js
@@ -134,7 +134,6 @@ app.factory('serviceDetails', ['$http',function($http){
             fd.append('image', cover);
 
         for(var i in oeuvre.newChapters) {
-            alert(oeuvre.newChapters[i].cover);
             if(oeuvre.newChapters[i].cover) {
                 fd.append('image-' + i, oeuvre.newChapters[i].cover);
                 oeuvre.newChapters[i].cover = undefined;
